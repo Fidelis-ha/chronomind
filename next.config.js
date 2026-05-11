b7941fe71fc181cca7daedbc100ce8109fc305d4
Fidelis <fidelis@hermes>
2026-05-11 12:56:35 +0000

    chore: re-enable serverActions with allowedOrigins

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['chronomind.vercel.app', 'localhost']
+    }
+  },
   images: {
     remotePatterns: [
       {
